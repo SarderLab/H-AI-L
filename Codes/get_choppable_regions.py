e227afea77cb1a6edc2964943a0e2dec8574d548
Brendon Lutnick <brendonl@buffalo.edu>
2019-03-08 15:50:00 -0500

    Add files via upload

--- Codes/get_choppable_regions.py
+++ Codes/get_choppable_regions.py
@@ -5,7 +5,7 @@ from skimage.filters import threshold_otsu
 from skimage.morphology import binary_closing, disk, remove_small_objects,label
 from scipy.ndimage.morphology import binary_fill_holes
 
-def get_choppable_regions(wsi,index_x, index_y, boxSize, object_size=150, se=disk(15)):
+def get_choppable_regions(wsi,index_x, index_y, boxSize):
     if wsi.split('.')[-1] != 'tif':
         slide=getWsi(wsi)
         slide_level = slide.level_count-1
@@ -18,12 +18,13 @@ def get_choppable_regions(wsi,index_x, index_y, boxSize, object_size=150, se=dis
         grayImage=0.2125*Im[:,:,0]+0.7154*Im[:,:,1]+0.0721*Im[:,:,2]
         grayImage[grayImage == 0] = 255
 
-        thresh = threshold_otsu(grayImage)
+        #thresh = threshold_otsu(grayImage)
+        thresh = 240
         binary =grayImage<thresh
         binary=binary_fill_holes(binary)
-        binary_closed=binary_closing(binary,se)
-        binary_closed=binary_fill_holes(binary_closed)
-        binary_closed=remove_small_objects(binary_closed,object_size)
+        #binary=binary(binary,se)
+        #binary=binary_fill_holes(binary)
+        #binary=remove_small_objects(binary,object_size)
 
         choppable_regions=np.zeros((len(index_y),len(index_x)))
         for idxy,yi in enumerate(index_y):
@@ -32,7 +33,7 @@ def get_choppable_regions(wsi,index_x, index_y, boxSize, object_size=150, se=dis
                 yStop = int(np.round((yi+boxSize)/resRatio))
                 xStart = int(np.round((xj)/resRatio))
                 xStop = int(np.round((xj+boxSize)/resRatio))
-                if np.sum(binary_closed[yStart:yStop,xStart:xStop])>0:
+                if np.sum(binary[yStart:yStop,xStart:xStop])>0:
                     choppable_regions[idxy,idxx]=1
 
     else:
